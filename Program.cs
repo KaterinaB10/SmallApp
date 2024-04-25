0d57f03ba9ace99cbbe7763e2612549793cb402d
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-25 13:03:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project {
+
+class Program
+{
+    static void Main()
+    {
+
+      Console.Write("Write down your age");
+      double userInput;
+      userInput = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("My age is " + userInput);
+
+
+    }
+}
+
+}
