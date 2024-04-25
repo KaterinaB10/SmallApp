c3768ef156fb985cda025a23942774420786fd45
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-04-25 14:56:34 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -7,24 +7,30 @@ class Program
     static void Main()
     {
 
-      Console.Write("Write down your age");
-      double userInput;
-      userInput = Convert.ToDouble(Console.ReadLine());
-      Console.WriteLine("My age is " + userInput);
+    //   Console.Write("Write down your age");
+    //   double userInput;
+    //   userInput = Convert.ToDouble(Console.ReadLine());
+    //   Console.WriteLine("My age is " + userInput);
 
     
-      Console.Write("Enter your role");
-      string? role;
-      role = Console.ReadLine();
+    //   Console.Write("Enter your role");
+    //   string? role;
+    //   role = Console.ReadLine();
 
 
-      if (role == "admin" && userInput > 18) {
-        Console.WriteLine("Hello, admin");
-      } else if  (role == "admin" && userInput < 18) {
-        Console.WriteLine("Hello, you are not admin");
-      } else Console.WriteLine("You don't have access");
+    //   if (role == "admin" && userInput > 18) {
+    //     Console.WriteLine("Hello, admin");
+    //   } else if  (role == "admin" && userInput < 18) {
+    //     Console.WriteLine("Hello, you are not admin");
+    //   } else Console.WriteLine("You don't have access");
 
-   
+    short[] numbers = new short[10];
+    Random randomNum = new Random();
+
+    for(byte i = 0; i < numbers.Length; i++) {
+        numbers[i] = Convert.ToInt16(randomNum.Next(2,15));
+        Console.WriteLine("Element: " + numbers[i]);
+    }
 
     }
 }
--- obj/Debug/net8.0/SmallApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmallApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81411d01c3b56aa681a98c3bd4dea273c4284c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b8a00a4385714256e2720e23d7d0198904c2aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
